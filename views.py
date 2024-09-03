270bce3d5a699878bc07268145b0eb87e105755b
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-09-03 17:45:07 -0500

    Support /test_stories POST request

--- views.py
+++ views.py
@@ -303,7 +303,7 @@ def forward_request(request_path):
         try:
             logger.debug(f'{g.app_request_id}: getting response from worker')
 
-            if request.method != 'GET' and any([request.path.startswith(path) for path in {'/text', '/searches', '/users'}]):
+            if request.method != 'GET' and any([request.path.startswith(path) for path in {'/text', '/searches', '/users', '/test_stories'}]):
                 session_method = getattr(worker_host.get('session'), request.method.lower())
                 worker_response = session_method(worker_url, json=request.json,
                                                              headers=worker_headers,
